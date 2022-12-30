1157b388f831f07fb50a67722c2a01f48f0f0930
nicohsfu <marcnicohernandez@gmail.com>
2022-12-30 01:13:06 -0800

    finish routes

--- backend/routes/tierListRoutes.js
+++ backend/routes/tierListRoutes.js
@@ -1,11 +1,68 @@
 const express = require("express");
-const userInterestController = require("../controllers/userInterestController.js");
+import { getAllTierLists, getTierListsById, deleteTierListById, createTierList, updateTierList } from "../db/queries/tier_lists";
 
 const router = express.Router();
 
-router.get("/", userInterestController.getAllUserInterests);
-router.get("/:id", (req,res) => userInterestController.getUserInterestsById(req.params.id).then((interests) => {res.send(interests)}))
-router.post("/", userInterestController.updateUserInterests);
+router.get("/", (req, res) => {
+  const tierLists = getAllTierLists()
+    .then((tierLists) => {
+      res.json(tierLists);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  const tierLists = getTierListsById(id)
+    .then((tierLists) => {
+      res.json(tierLists);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.get("/:id/delete", (req, res) => {
+  const id = req.params.id;
+
+  const tierLists = deleteTierListById(id)
+    .then((tierLists) => {
+      res.json(tierLists);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.post("/:id/new", (req, res) => {
+  const id = req.params.id;
+  const tierListInfo = req.body;
+
+  console.log("tierListInfo", tierListInfo)
+
+  const tierLists = createTierList(id, tierListInfo)
+    .then((tierLists) => {
+      res.json(tierLists);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.put("/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const tierListInfo = req.body;
+
+  const tierList = updateTierList(id, tierListInfo)
+    .then((tierList) => {
+      res.json(tierList);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,24 +1,64 @@
 const express = require("express");
-const userController = require("../controllers/userController.js");
-const userInterestController = require("../controllers/userInterestController.js");
+import { getAllUsers, getUserById, addUser, updateUser } from "../db/queries/users";
 
 const router = express.Router();
 
-router.get("/", userController.getAllUsers);
-router.post("/", userController.addUser);
-router.get("/logout", userController.Logout);
-router.get("/:id", (req, res) =>
-  userController.getUserById(req.params.id).then((user) => {
-    res.send(user);
-  })
-);
-router.get("/:id/common", (req,res) => {
-  userController.getUserCommon(req.params.id).then((user) => {
-    res.send(user);
-  })
-})
+router.get("/", (req, res) => {
+  const users = getAllUsers()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 
-router.post("/update", userController.addUserProfileInfo);
-router.post("/login", userController.Login);
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const user = getUserById(id)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.post("/:id/new", (req, res) => {
+  const id = req.params.id;
+
+  console.log("req.body", req.body);
+  const tierItem = addTierItem(id, req.body)
+    .then((tierItem) => {
+      res.json(tierItem);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  const tierItem = deleteTierItem(id)
+    .then((tierItem) => {
+      res.json(tierItem);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.put('/:id/edit', (req, res) => {
+  const id = req.params.id;
+
+  const tierItem = updateTierItem(id, tierItemInfo)
+    .then((tierItem) => {
+      res.json(tierItem);
+    })
+    .catch(
+      res.status(500).json({ error: err.message })
+    );
+});
 
 module.exports = router;
